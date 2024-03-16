16a3090b8061b62a5a421b4ca014e7ae1eaec14d
Shubham Dubey <itzshubhamofficial@gmail.com>
2024-03-16 15:28:26 +0530

    Database String Changed to MongoDB Atlas Cloud

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection("mongodb://localhost:27017/FixItParts").on("open", ()=>{
+const connection = mongoose.createConnection("mongodb+srv://itzshubhamofficial:Shubham%401166@cluster444.acgkmtb.mongodb.net/FixItParts").on("open", ()=>{
     console.log("Database Connected");
 });
 
