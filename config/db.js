c8568578588f7e6d63c1a0df3ae71b42ce379509
Shubham Dubey <itzshubhamofficial@gmail.com>
2024-03-17 19:28:08 +0530

    database string updated new

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection("mongodb+srv://itzshubhamofficial:Shubham%401166@cluster444.acgkmtb.mongodb.net/FixItParts").on("open", ()=>{
+const connection = mongoose.createConnection("mongodb+srv://shubham_dubey:Shubham%401166@mongoserver.jhrakvz.mongodb.net/?retryWrites=true&w=majority&appName=MongoServer/FixItParts").on("open", ()=>{
     console.log("Database Connected");
 });
 
